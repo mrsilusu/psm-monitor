5a9a1013e4de1087ce1ffb3f58b72e0b788d57fb
Nsimba Silusu <bernardosilusu@gmail.com>
2026-01-14 21:56:34 +0000

    Atualização de ficheiro 2.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; // Isto vai carregar o seu ficheiro App.jsx
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
